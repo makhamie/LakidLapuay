7a0d7093cfb9f010a1147a5e9b503208d9a9a36e
Arut Thanomwatana <eaglization@gmail.com>
2018-05-14 20:27:39 +0700

    Supervisor can create task

--- src/components/SupervisorNavbar.vue
+++ src/components/SupervisorNavbar.vue
@@ -7,7 +7,7 @@
     text-color="#fff"
     active-text-color="#ffd04b" router>
       <div class="navbar-brand">LakidLapuay</div>
-      <!-- <el-menu-item index="Admin" :route="{name: 'Admin'}">Create User</el-menu-item> -->
+      <el-menu-item index="Supervisor" :route="{name: 'Supervisor'}">Create Task</el-menu-item>
       <!-- <el-menu-item index="ManageUser" :route="{name: 'ManageUser'}">Manage User Account</el-menu-item> -->
       <!-- <el-menu-item index="CreateDepartment" :route="{name: 'CreateDepartment'}">Create Department</el-menu-item> -->
       <el-menu-item @click="$emit('logout')" v-if="isLogin" index="Login" :route="{name: 'Login'}" class="pull-right">Logout</el-menu-item>
--- src/resources/SupervisorProvider.js
+++ src/resources/SupervisorProvider.js
@@ -1,7 +1,19 @@
 import HttpRequest from './HttpRequest'
 
 class SupervisorProvider extends HttpRequest {
+  getSubordinators () {
+    return this.fetch('/get-subordinates')
+  }
 
+  createTask (subordinateId, name, startedAt, finishedAt, description) {
+    return this.create('/task', {
+      subordinate_id: subordinateId,
+      name,
+      started_at: startedAt,
+      finished_at: finishedAt,
+      description
+    })
+  }
 }
 
 export default SupervisorProvider
